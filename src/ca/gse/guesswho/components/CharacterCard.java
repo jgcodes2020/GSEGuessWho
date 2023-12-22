8bdd39e3e0ebefd9866e9bde9da218a2357756d3
jgcodes2020 <jackyguo18@hotmail.com>
2023-12-22 14:57:59 -0500

    character select events

--- src/ca/gse/guesswho/components/CharacterCard.java
+++ src/ca/gse/guesswho/components/CharacterCard.java
@@ -2,24 +2,31 @@ package ca.gse.guesswho.components;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Image;
+import java.util.function.Consumer;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.util.List;
+import java.util.ArrayList;
 
+import ca.gse.guesswho.events.CharacterSelectEvent;
 import ca.gse.guesswho.models.GuessWhoCharacter;
 
 public class CharacterCard extends JPanel {
 	// even though Java was developed by Americans, and spells "colour" that way
 	// I refuse to spell it the wrong way.
 	private static Color HIGHLIGHT_COLOUR = new Color(0x0088FF);
+	private static Color SELECT_COLOUR = 	new Color(0x85C6FF);
 	
 	private JLabel textLabel;
 	private CharacterImageDisplay iconLabel;
-	private boolean clickable = true;
+	private boolean clickable;
+	private boolean selected;
+	
+	private List<Consumer<CharacterSelectEvent>> selectHandlers = new ArrayList<>();
 	
 	public CharacterCard(GuessWhoCharacter character) {
 		setLayout(new BorderLayout());
@@ -47,6 +54,13 @@ public class CharacterCard extends JPanel {
 			public void mouseExited(MouseEvent e) {
 				CharacterCard.this.handleMouseExited(e);
 			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				for (Consumer<CharacterSelectEvent> handler : CharacterCard.this.selectHandlers) {
+					handler.accept(new CharacterSelectEvent(CharacterCard.this));
+				}
+			}
 		});
 	}
 	
@@ -63,14 +77,21 @@ public class CharacterCard extends JPanel {
 	private void handleMouseExited(MouseEvent e) {
 		this.setBackground(Color.WHITE);
 		this.repaint();
-		
 	}
-
+	
 	public boolean isClickable(){
 		return clickable;
 	}
 
 	public void setClickable(boolean state) {
 		clickable = state;
+	}
+	
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
 	}
 }
