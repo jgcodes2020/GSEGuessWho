31b8b88800313340a6ef76c02fc6cd907f202261
Winston Zhao <winstonzhao.ca@outlook.com>
2023-12-22 13:37:22 -0500

    trying to print but kinda broken

--- src/ca/gse/guesswho/models/players/DumbAIPlayer.java
+++ src/ca/gse/guesswho/models/players/DumbAIPlayer.java
@@ -3,6 +3,7 @@ package ca.gse.guesswho.models.players;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 import ca.gse.guesswho.models.DataCaches;
 import ca.gse.guesswho.models.GameState;
@@ -15,6 +16,7 @@ import ca.gse.guesswho.models.questions.CharacterQuestion;
 public class DumbAIPlayer extends Player {
 	private Random rng;
 	private HashSet<Question> previousQuestions;
+  private Scanner scanner = new Scanner(System.in);
 
 	/**
 	 * Creates a new dumb AI player.
@@ -23,6 +25,18 @@ public class DumbAIPlayer extends Player {
 		rng = new Random();
 		previousQuestions = new HashSet<>();
 	}
+
+  public static void main(String[] args) {
+    DumbAIPlayer player = new DumbAIPlayer();
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+        System.out.println("It's your turn, " + player.getName());
+        Question question = player.takeTurn(scanner);
+        System.out.println("Your question: " + question);
+        // ... handle the answer and game logic ...
+    }
+  }
 
 	/**
 	 * {@inheritDoc}
@@ -35,25 +49,26 @@ public class DumbAIPlayer extends Player {
 	 */
 	@Override
 	public Question takeTurn() {
-		List<GuessWhoCharacter> characters = DataCaches.getCharacterList();
+    List<GuessWhoCharacter> characters = DataCaches.getCharacterList();
 
-		// if we're down to one option, guess that one
-		if (remainingIndexes.cardinality() == 1) {
-			int finalIndex = remainingIndexes.nextSetBit(0);
-			return new CharacterQuestion(characters.get(finalIndex));
-		}
-		
-		Question nextQuestion;
-		do {
-			// generate a random attribute question
-			int randomAttribute = rng.nextInt(GuessWhoCharacter.ATTRIBUTE_NUM_VALS);
-			byte randomValue = (byte) rng.nextInt(GuessWhoCharacter.attributeMaxValue(randomAttribute));
-			nextQuestion = new AttributeQuestion(randomAttribute, randomValue);
-			// if it has been asked before, ask a different one (hopefully this doesn't hang or something)
-		} while (previousQuestions.contains(nextQuestion));
-		// add this question to the list of previously-asked questions
-		previousQuestions.add(nextQuestion);
-		return nextQuestion;
+    // if we're down to one option, guess that one
+    if (remainingIndexes.cardinality() == 1) {
+        int finalIndex = remainingIndexes.nextSetBit(0);
+        return new CharacterQuestion(characters.get(finalIndex));
+    }
+    
+    Question nextQuestion;
+    do {
+        System.out.println("Enter attribute number:");
+        int randomAttribute = scanner.nextInt();
+        System.out.println("Enter attribute value:");
+        byte randomValue = scanner.nextByte();
+        nextQuestion = new AttributeQuestion(randomAttribute, randomValue);
+        // if it has been asked before, ask a different one (hopefully this doesn't hang or something)
+    } while (previousQuestions.contains(nextQuestion));
+    // add this question to the list of previously-asked questions
+    previousQuestions.add(nextQuestion);
+    return nextQuestion;
 	}
 
 	/**
@@ -66,9 +81,12 @@ public class DumbAIPlayer extends Player {
 	}
 
 	@Override
-	public String getName() {
-		// TODO: should we add actual names or leave this one?
-		return "NoobGamer69420";
-	}
+  public String getName() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your name:");
+    String name = scanner.nextLine();
+    scanner.close();
+    return name;
+  }
 
 }
