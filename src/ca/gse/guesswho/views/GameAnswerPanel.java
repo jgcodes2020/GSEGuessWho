804d491c38a22f6692fac48aa9a7be53479fcbe6
jgcodes2020 <jackyguo18@hotmail.com>
2024-01-09 14:48:21 -0500

    answer panel

--- src/ca/gse/guesswho/views/GameAnswerPanel.java
+++ src/ca/gse/guesswho/views/GameAnswerPanel.java
@@ -1,17 +1,30 @@
 package ca.gse.guesswho.views;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import ca.gse.guesswho.models.DataCaches;
+import ca.gse.guesswho.models.GameState;
+import ca.gse.guesswho.models.Question;
+import ca.gse.guesswho.models.players.HumanPlayer;
+
 public class GameAnswerPanel extends JPanel {
     private GamePanel parent;
+	
     private JLabel questionLabel;	
+    private String questionStored;
+	
     private JPanel responsePanel;
 	private JPanel questionPanel;
-    private String questionStored;
+	
+	private JButton yesButton;
+	private JButton noButton;
 
     /**
 	 * Constructs a Answer Panel (Panel for responding yes or no to questions)
@@ -23,6 +36,15 @@ public class GameAnswerPanel extends JPanel {
 		setLayout(new BorderLayout());
 		add(responsePanel, BorderLayout.CENTER);
 		add(questionPanel, BorderLayout.NORTH);
+		
+		this.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent e) {
+				GamePanel parent = GameAnswerPanel.this.parent;
+				Question question = parent.getState().getLastQuestion();
+				setQuestion(DataCaches.getQuestionString(question));
+			}
+		});
     }
 
 
@@ -45,12 +67,28 @@ public class GameAnswerPanel extends JPanel {
 	 */
     private JPanel buildResponseBar(){
         JPanel board = new JPanel();
-        JButton yesButton = new JButton("Yes");
-        JButton noButton = new JButton("No");
+        yesButton = new JButton("Yes");
+        noButton = new JButton("No");
         board.add(yesButton);
         board.add(noButton);
         return board;
     }
+	
+	private void onResponseClicked(ActionEvent e) {
+		Object source = e.getSource();
+		
+		GameState state = parent.getState();
+		HumanPlayer player = (HumanPlayer) state.getCurrentPlayer();
+		if (source == yesButton) {
+			player.setNextAnswer(true);
+		}
+		else if (source == noButton) {
+			player.setNextAnswer(false);
+		}
+		state.doNextTurn();
+		parent.runAITurnsAndSwitchPanel();
+	}
+	
 	/**
      * Set the question on the top bar
 	 * 
