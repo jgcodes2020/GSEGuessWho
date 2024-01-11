661cb01124086fc4e86dc8c584e4cb9bf4f1a82f
jgcodes2020 <jackyguo18@hotmail.com>
2024-01-11 15:37:15 -0500

    replace cross-out with graying out

--- src/ca/gse/guesswho/components/CharacterImageDisplay.java
+++ src/ca/gse/guesswho/components/CharacterImageDisplay.java
@@ -1,12 +1,18 @@
 package ca.gse.guesswho.components;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
+import java.awt.image.ImageProducer;
 
+import javax.swing.GrayFilter;
 import javax.swing.JComponent;
 
 /**
@@ -15,11 +21,14 @@ import javax.swing.JComponent;
  */
 public class CharacterImageDisplay extends JComponent {
 	private Image image;
+	private Image grayImage;
 	private boolean crossedOut;
 
 	public CharacterImageDisplay(Image image) {
 		this.image = image;
 		this.crossedOut = false;
+		
+		updateGrayImage();
 	}
 	
 	/**
@@ -35,6 +44,16 @@ public class CharacterImageDisplay extends JComponent {
 	 */
 	public void setImage(Image image) {
 		this.image = image;
+		updateGrayImage();
+	}
+	
+	private void updateGrayImage() {
+		// this is an object which applies a graying filter
+		ImageProducer producer = new FilteredImageSource(
+			image.getSource(), new GrayFilter(true, 50)
+		);
+		// this applies the filter
+		grayImage = Toolkit.getDefaultToolkit().createImage(producer);
 	}
 
 	/**
@@ -75,24 +94,25 @@ public class CharacterImageDisplay extends JComponent {
 		// this cast *shouldn't* fail
 		Graphics2D g2d = (Graphics2D) g;
 		
-		if (image == null)
+		Image curImage;
+		if (crossedOut)
+			curImage = grayImage;
+		else
+			curImage = image;
+		
+		if (curImage == null)
 			return;
 		
 		// calculate the bounds needed to preserve the icon's aspect ratio
 		Dimension panelSize = getSize();
-		Dimension imageSize = new Dimension(image.getWidth(this), image.getHeight(this));
+		Dimension imageSize = new Dimension(curImage.getWidth(this), curImage.getHeight(this));
 		Rectangle scaledRect = Geometry.sizeToFitBounds(panelSize, imageSize);
+		
+		
         
 		// Scale and draw the image
 		g2d.drawImage(
-			image.getScaledInstance(scaledRect.width, scaledRect.height, Image.SCALE_AREA_AVERAGING), 
+			curImage.getScaledInstance(scaledRect.width, scaledRect.height, Image.SCALE_AREA_AVERAGING), 
 			scaledRect.x, scaledRect.y, scaledRect.width, scaledRect.height, null, this);
-		
-		// if it's crossed out draw a big ol' X over it.
-		if (crossedOut) {
-			g2d.setStroke(new BasicStroke(5.0f));
-			g2d.drawLine(scaledRect.x, scaledRect.y, scaledRect.x + scaledRect.width, scaledRect.y + scaledRect.height);
-			g2d.drawLine(scaledRect.x + scaledRect.width, scaledRect.y, scaledRect.x, scaledRect.y + scaledRect.height);
-		}
 	}
 }
